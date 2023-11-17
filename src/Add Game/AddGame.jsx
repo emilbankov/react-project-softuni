f10617709d141ddaed68e31010b85207def239ec
emilbankov <emilbankov17d@gmail.com>
2023-11-17 21:09:39 +0200

    Added AddGame Component and CSS

---
+++ src/Add Game/AddGame.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import './AddGame.module.css'
+
+export default function AddGame() {
+    const [title, setTitle] = useState('');
+    const [genre, setGenre] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+    const [description, setDescription] = useState('');
+    const [developer, setDeveloper] = useState('');
+    const [price, setPrice] = useState('');
+
+    const handleAddGame = (e) => {
+        e.preventDefault();
+        // TODO
+    };
+
+    return (
+        <div className="add-game-page">
+            <div className="add-game-container">
+                <h2>Add Game</h2>
+                <form className="add-game-form" onSubmit={handleAddGame}>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        type="text"
+                        id="title"
+                        placeholder="Enter game title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+
+                    <label htmlFor="genre">Genre:</label>
+                    <input
+                        type="text"
+                        id="genre"
+                        placeholder="Enter game genre"
+                        value={genre}
+                        onChange={(e) => setGenre(e.target.value)}
+                    />
+
+                    <label htmlFor="imageUrl">Image URL:</label>
+                    <input
+                        type="text"
+                        id="imageUrl"
+                        placeholder="Enter game image URL"
+                        value={imageUrl}
+                        onChange={(e) => setImageUrl(e.target.value)}
+                    />
+
+                    <label htmlFor="description">Description:</label>
+                    <textarea
+                        id="description"
+                        placeholder="Enter game description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    ></textarea>
+
+                    <label htmlFor="developer">Developer:</label>
+                    <input
+                        type="text"
+                        id="developer"
+                        placeholder="Enter game developer"
+                        value={developer}
+                        onChange={(e) => setDeveloper(e.target.value)}
+                    />
+
+                    <label htmlFor="price">Price:</label>
+                    <input
+                        type="text"
+                        id="price"
+                        placeholder="Enter game price"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
+
+                    <button type="submit">Add Game</button>
+                </form>
+            </div>
+        </div>
+    );
+}
