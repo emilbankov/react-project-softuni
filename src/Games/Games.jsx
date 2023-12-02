5dbba05e9f682d73f4694085291a5b5a1c59aee5
emilbankov <emilbankov17d@gmail.com>
2023-12-02 22:36:11 +0200

    Added example Catalog page

--- src/Games/Games.jsx
+++ src/Games/Games.jsx
@@ -1,6 +1,139 @@
+import { Link } from "react-router-dom"
+
 export default function Games() {
     return (
         <>
+            <div className="grid-container">
+                {/* First Row */}
+                <div className="card-container">
+                    <div className="card-image-container">
+                        <img
+                            src="https://www.albagame.al/cdn/shop/files/135179.png?v=1686908142"
+                            alt="Card"
+                            className="card-image"
+                        />
+                        <div className="card-overlay">
+                            <h2 className="card-title">Spider-Man</h2>
+                        </div>
+                    </div>
+                    <div className="card-price-container">
+                        <p className="card-price">Price: $19.99</p>
+                    </div>
+                </div>
+
+                <div className="card-container">
+                    <div className="card-image-container">
+                        <img
+                            src="https://www.albagame.al/cdn/shop/files/135179.png?v=1686908142"
+                            alt="Card"
+                            className="card-image"
+                        />
+                        <div className="card-overlay">
+                            <h2 className="card-title">Spider-Man</h2>
+                        </div>
+                    </div>
+                    <div className="card-price-container">
+                        <p className="card-price">Price: $19.99</p>
+                    </div>
+                </div>
+
+                <div className="card-container">
+                    <div className="card-image-container">
+                        <img
+                            src="https://www.albagame.al/cdn/shop/files/135179.png?v=1686908142"
+                            alt="Card"
+                            className="card-image"
+                        />
+                        <div className="card-overlay">
+                            <h2 className="card-title">Spider-Man</h2>
+                        </div>
+                    </div>
+                    <div className="card-price-container">
+                        <p className="card-price">Price: $19.99</p>
+                    </div>
+                </div>
+
+                <div className="card-container">
+                    <div className="card-image-container">
+                        <img
+                            src="https://www.albagame.al/cdn/shop/files/135179.png?v=1686908142"
+                            alt="Card"
+                            className="card-image"
+                        />
+                        <div className="card-overlay">
+                            <h2 className="card-title">Spider-Man</h2>
+                        </div>
+                    </div>
+                    <div className="card-price-container">
+                        <p className="card-price">Price: $19.99</p>
+                    </div>
+                </div>
+
+                {/* Second Row */}
+                <div className="card-container">
+                    <div className="card-image-container">
+                        <img
+                            src="https://www.albagame.al/cdn/shop/files/135179.png?v=1686908142"
+                            alt="Card"
+                            className="card-image"
+                        />
+                        <div className="card-overlay">
+                            <h2 className="card-title">Spider-Man</h2>
+                        </div>
+                    </div>
+                    <div className="card-price-container">
+                        <p className="card-price">Price: $19.99</p>
+                    </div>
+                </div>
+
+                <div className="card-container">
+                    <div className="card-image-container">
+                        <img
+                            src="https://www.albagame.al/cdn/shop/files/135179.png?v=1686908142"
+                            alt="Card"
+                            className="card-image"
+                        />
+                        <div className="card-overlay">
+                            <h2 className="card-title">Spider-Man</h2>
+                        </div>
+                    </div>
+                    <div className="card-price-container">
+                        <p className="card-price">Price: $19.99</p>
+                    </div>
+                </div>
+
+                <div className="card-container">
+                    <div className="card-image-container">
+                        <img
+                            src="https://www.albagame.al/cdn/shop/files/135179.png?v=1686908142"
+                            alt="Card"
+                            className="card-image"
+                        />
+                        <div className="card-overlay">
+                            <h2 className="card-title">Spider-Man</h2>
+                        </div>
+                    </div>
+                    <div className="card-price-container">
+                        <p className="card-price">Price: $19.99</p>
+                    </div>
+                </div>
+
+                <Link to={'/'}><div className="card-container">
+                    <div className="card-image-container">
+                        <img
+                            src="https://www.albagame.al/cdn/shop/files/135179.png?v=1686908142"
+                            alt="Card"
+                            className="card-image"
+                        />
+                        <div className="card-overlay">
+                            <h2 className="card-title">Spider-Man</h2>
+                        </div>
+                    </div>
+                    <div className="card-price-container">
+                        <p className="card-price">Price: $19.99</p>
+                    </div>
+                </div></Link>
+            </div>
             <div className="gallery-w3layouts" id="gallery">
                 <div className="container">
                     <h2>Exclusive Games</h2>
