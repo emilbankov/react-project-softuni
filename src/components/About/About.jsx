bf67ebaec1aced1c689e80c5b8c3691377a61fa3
emilbankov <emilbankov17d@gmail.com>
2023-12-07 03:54:50 +0200

    Updated About page and added new css

---
+++ src/components/About/About.jsx
@@ -0,0 +1,83 @@
+import './About.module.css';
+
+export default function About() {
+    return (
+        <>
+            <div className="about-agileinfo" id="about">
+                <h3>About Us</h3>
+                <div className="about-container">
+                    <div className='about-us-info'>
+                        <div className="about-top-left">
+
+                        </div>
+                        <div className="about-bottom-right">
+                            <span className='about-heading'>History</span>
+                            <span className='about-sub-heading'>"Established in 2020"</span>
+                            <p>Greetings to our gaming haven, where passion meets pixels! Since 2020, our digital realm was conceived from a shared love for immersive gaming experiences and cutting-edge technology. Our journey began with a vision to create a space where gamers of all levels could find their perfect match. From high-powered rigs to retro classics, we curate a diverse collection that caters to every gaming palate. Fueled by a relentless dedication to quality and innovation, our site is more than just a store - it is a virtual realm where gamers unite. Join us on this epic adventure, where every click tells a story.</p>
+                        </div>
+                    </div>
+
+                    <div className="clearfix"></div>
+                </div>
+            </div>
+
+            <div className="collection-agileits">
+                <h2>Fun Facts About Games</h2>
+                <div className="collection-grids-agileits-w3layouts">
+                    <div className="col-md-4 col-sm-4 collection-left"></div>
+                    <div className="col-md-8 col-sm-8 collection-right">
+                        <div className="col-md-4 col-sm-4 collection-grid collection-text collection-grid-1">
+                            <img src="/images/c1.png" alt="" />
+                            <h3>Super Mario </h3>
+                            <p>Mario was originally known as "Jumpman" in Donkey Kong. He got his iconic name from Nintendo's warehouse landlord, Mario Segale.</p>
+                        </div>
+                        <div className="col-md-4 col-sm-4 collection-grid collection-image collection-grid-2"></div>
+
+                        <div className="col-md-4 col-sm-4 collection-grid collection-text collection-grid-3">
+                            <img src="/images/c2.png" alt="" />
+                            <h3>Pac Man</h3>
+                            <p>The ghosts in Pac Man have distinct personalities. Blinky is programmed to chase you, Pinky tries to ambush, Inky is random, and Clyde alternates between random and chasing.</p>
+                        </div>
+                        <div className="col-md-4 col-sm-4 collection-grid collection-image collection-grid-4"></div>
+
+                        <div className="col-md-4 col-sm-4 collection-grid collection-text collection-grid-5">
+                            <img src="/images/c3.png" alt="" />
+                            <h3>Minecraft</h3>
+                            <p>The world in Minecraft is virtually infinite. If you were to walk from one end to the other without stopping, it would take over 820 hours in real time.</p>
+                        </div>
+                        <div className="col-md-4 col-sm-4 collection-grid collection-image collection-grid-6"></div>
+
+                        <div className="clearfix"></div>
+                    </div>
+                    <div className="clearfix"></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+{/* <h3>Welcome to Infinite Pixels: Your Gateway to Gaming Excellence</h3>
+
+<div className='about'>
+    <section>
+        <p>Infinite Pixels was born out of a shared love for the captivating world of video games. As avid gamers ourselves, we recognized the need for a platform that goes beyond the ordinary—an online haven where gamers of all levels can discover, explore, and indulge in the latest and greatest titles.</p>
+        <p>Driven by a commitment to quality, innovation, and customer satisfaction, Infinite Pixels was conceptualized to redefine the way you experience gaming. We understand that every gamer is unique, and our collection reflects this diversity. From action-packed blockbusters to indie gems, we curate a selection that caters to every taste and preference.</p>
+    </se    
+    <section>
+        <h3>What Sets Us Apart</h3>
+        <ul>
+            <li><strong>Curated Selection:</strong> Our team of gaming enthusiasts scours the digital landscape to bring you a carefully curated selection of titles that span genres, platforms, and playstyles.</li>
+            <li><strong>Exceptional Service:</strong> We don't just sell games; we provide an unparalleled shopping experience. Our commitment to customer service means you can shop with confidence, knowing that your satisfaction is our top priority.</li>
+            <li><strong>Infinite Possibilities:</strong> True to our name, we believe in infinite possibilities. From classic favorites to cutting-edge releases, we embrace the ever-evolving landscape of gaming.</li>
+        </ul>
+    </se    
+    <section>
+        <h3>Join the Infinite Pixels Community</h3>
+        <p>Infinite Pixels is more than just a store; it's a community of like-minded individuals who share a passion for gaming. Connect with us on social media, join discussions, and stay informed about the latest releases, updates, and gaming events. We're not just selling games; we're building a community where every pixel counts.</p>
+    </se    
+    <section>
+        <h3>Thank You for Choosing Infinite Pixels</h3>
+        <p>Whether you're a seasoned gamer or just starting your digital journey, we extend our heartfelt gratitude for choosing Infinite Pixels as your gaming destination. We are committed to providing you with an exceptional gaming experience, and we look forward to being a part of your gaming adventures.</p>
+        <p>Welcome to Infinite Pixels—where the possibilities are endless, and the pixels are infinite.</p>
+    </section>
+</div>  */}
