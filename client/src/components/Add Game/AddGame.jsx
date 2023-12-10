30cc56bf286c34aa5e5cdd94d3b34cabd3ed906f
emilbankov <emilbankov17d@gmail.com>
2023-12-10 07:07:58 +0200

    Added add game form validation

--- client/src/components/Add Game/AddGame.jsx
+++ client/src/components/Add Game/AddGame.jsx
@@ -1,10 +1,63 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import './AddGame.module.css'
 import AuthContext from '../../contexts/AuthContext.js';
 import useForm from '../../hooks/useForm.js';
 
 export default function AddGame() {
     const { createGameHandler } = useContext(AuthContext);
+    const [errors, setErrors] = useState({});
+
+    const validate = (values) => {
+        const newErrors = {};
+
+        if (!values.title) {
+            newErrors.title = "Title is required!";
+        } else if (values.title.length < 5) {
+            newErrors.title = "Title must be at least 5 characters!";
+        }
+
+        if (!values.imageUrl) {
+            newErrors.imageUrl = "Image URL is required!";
+        } else if (!/^https?:\/\/.+/.test(values.imageUrl)) {
+            newErrors.imageUrl = "Provide a valid image URL!";
+        }
+
+        if (!values.genre) {
+            newErrors.genre = "Genre is required!";
+        } else if (values.genre.length < 3) {
+            newErrors.genre = "Genre must be at least 3 characters!";
+        }
+
+        if (!values.developer) {
+            newErrors.developer = "Developer is required!";
+        } else if (values.developer.length < 3) {
+            newErrors.developer = "Developer must be at least 3 characters!";
+        }
+
+        if (!values.players) {
+            newErrors.players = "Players is required!";
+        } else if (values.players < 0) {
+            newErrors.players = "Players must be positive number!";
+        }
+
+        if (!values.price) {
+            newErrors.price = "Price is required!";
+        } else if (!values.price.includes(",")) {
+            newErrors.price = "Price must be number,number!";
+        }
+
+        if (!values.description) {
+            newErrors.description = "Description is required!";
+        } else if (values.description.length < 10 || values.description.length > 1000) {
+            newErrors.description = "Description must be between 10 and 1000 characters!";
+        }
+
+        const firstErrorField = Object.keys(newErrors)[0];
+        setErrors(firstErrorField ? { [firstErrorField]: newErrors[firstErrorField] } : {});
+
+        return newErrors;
+    };
+
     const { values, onChange, onSubmit } = useForm(createGameHandler, {
         title: "",
         imageUrl: "",
@@ -12,8 +65,8 @@ export default function AddGame() {
         developer: "",
         players: "",
         price: "",
-        description: ""
-    })
+        description: "",
+    }, validate)
 
     return (
         <div className="add-game-page">
@@ -26,9 +79,10 @@ export default function AddGame() {
                         id="title"
                         name='title'
                         placeholder="Enter game title"
+                        value={values.title}
                         onChange={onChange}
-                        value={values.name}
                     />
+                    {errors.title && <div className="error-msg"><span>{errors.title}</span></div>}
 
                     <label htmlFor="imageUrl">Image URL:</label>
                     <input
@@ -36,9 +90,10 @@ export default function AddGame() {
                         id="imageUrl"
                         name='imageUrl'
                         placeholder="Enter game image URL"
-                        onChange={onChange}
                         value={values.imageUrl}
+                        onChange={onChange}
                     />
+                    {errors.imageUrl && <div className="error-msg"><span>{errors.imageUrl}</span></div>}
 
                     <label htmlFor="genre">Genre:</label>
                     <input
@@ -46,9 +101,10 @@ export default function AddGame() {
                         id="genre"
                         name='genre'
                         placeholder="Enter game genre"
-                        onChange={onChange}
                         value={values.genre}
+                        onChange={onChange}
                     />
+                    {errors.genre && <div className="error-msg"><span>{errors.genre}</span></div>}
 
                     <label htmlFor="developer">Developer:</label>
                     <input
@@ -56,9 +112,10 @@ export default function AddGame() {
                         id="developer"
                         name='developer'
                         placeholder="Enter game developer"
-                        onChange={onChange}
                         value={values.developer}
+                        onChange={onChange}
                     />
+                    {errors.developer && <div className="error-msg"><span>{errors.developer}</span></div>}
 
                     <label htmlFor="players">Players:</label>
                     <input
@@ -66,9 +123,10 @@ export default function AddGame() {
                         id="players"
                         name='players'
                         placeholder="Enter players count"
-                        onChange={onChange}
                         value={values.players}
+                        onChange={onChange}
                     />
+                    {errors.players && <div className="error-msg"><span>{errors.players}</span></div>}
 
                     <label htmlFor="price">Price:</label>
                     <input
@@ -76,18 +134,20 @@ export default function AddGame() {
                         id="price"
                         name='price'
                         placeholder="Enter game price"
-                        onChange={onChange}
                         value={values.price}
+                        onChange={onChange}
                     />
+                    {errors.price && <div className="error-msg"><span>{errors.price}</span></div>}
 
                     <label htmlFor="description">Description:</label>
                     <textarea
                         id="description"
                         name='description'
                         placeholder="Enter game description"
-                        onChange={onChange}
                         value={values.description}
+                        onChange={onChange}
                     ></textarea>
+                    {errors.description && <div className="error-msg"><span>{errors.description}</span></div>}
 
                     <button type="submit">Add Game</button>
                 </form>
