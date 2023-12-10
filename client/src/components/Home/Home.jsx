c3d5c05af417ec8c412ab5baaa34069ebc7d0ef0
emilbankov <emilbankov17d@gmail.com>
2023-12-10 07:03:34 +0200

    Updated Home component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,22 +1,32 @@
 import { useEffect } from "react"
+import { Link } from "react-router-dom";
+import MostAnticipated from "./MostAnticipated.jsx";
 
 export default function Home() {
     useEffect(() => {
         $("#flexiselDemo1").flexisel({
-          visibleItems: 4,
-          animationSpeed: 1000,
-          autoPlay: true,
-          autoPlaySpeed: 3000,
-          pauseOnHover: true,
+            visibleItems: 4,
+            animationSpeed: 1000,
+            autoPlay: true,
+            autoPlaySpeed: 3000,
+            pauseOnHover: true,
         });
-      }, []);
+    }, []);
 
 
     return (
         <>
+            <div className="trend-w3layouts most-anticipated-trend">
+                <h2>Most Anticipated Game</h2>
+                <div className="most-anticipated">
+                    <Link to={"/most-anticipated"}>
+                        <img src="https://insider-gaming.com/wp-content/uploads/2023/05/gta-6-1.jpg" alt="" />
+                    </Link>
+                </div>
+            </div>
             <div className="trend-w3layouts">
                 <div className="container">
-                    <h2>Trending Games</h2>
+                    <h2>Trending Genres</h2>
                     <ul id="flexiselDemo1">
                         <li>
                             <div className="trend-grid">
@@ -26,7 +36,7 @@ export default function Home() {
                         </li>
                         <li>
                             <div className="trend-grid">
-                                <h4>3D Games</h4>
+                                <h4>Kids Games</h4>
                                 <img src="/images/tg2.jpg" alt=" " className="img-responsive" />
                             </div>
                         </li>
@@ -38,7 +48,7 @@ export default function Home() {
                         </li>
                         <li>
                             <div className="trend-grid">
-                                <h4>Toy Games</h4>
+                                <h4>Fantasy Games</h4>
                                 <img src="/images/tg4.jpg" alt=" " className="img-responsive" />
                             </div>
                         </li>
